30ddfdd9254dd66607916ecf56a3169e7ea94399
Himayat <himayatu99@gmail.com>
2024-08-11 23:23:29 +0500

    Get request

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    // baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    fixturesFolder: 'cypress/fixtures',
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    env: {
+      apiUrl: 'http://localhost:8080/api'
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
---
+++ cypress/e2e/api_test.cy.js
@@ -0,0 +1,16 @@
+///<reference types='Cypress'/>
+
+describe('Request command suite', () => {
+  it('Get request', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:8080/todos',
+      qs: { "id": 5977 }
+    })
+      .then(response => {
+        // cy.log(response.body[0].name)
+        expect(response.status).to.be.eq(200)
+        expect(response.body[0].name).to.be.eq('Home')
+      })
+  })
+})
